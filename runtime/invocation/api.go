5048b0eb7dcafc0a0af3f936efd99decd29708a3
michimani <michimani210@gmail.com>
2022-11-18 23:42:28 +0900

    feature: POST /runtime/invocation/{AwsRequestId}/response API

--- runtime/invocation/api.go
+++ runtime/invocation/api.go
@@ -12,7 +12,8 @@ import (
 )
 
 const (
-	invocationNextEndpointFmt string = "http://%s/2018-06-01/runtime/invocation/next"
+	invocationNextEndpointFmt     string = "http://%s/2018-06-01/runtime/invocation/next"
+	invocationResponseEndpointFmt string = "http://%s/2018-06-01/runtime/invocation/%s/response"
 
 	// Response Header Names
 	responseHeaderLambdaRuntimeAwsRequestId       string = "Lambda-Runtime-Aws-Request-Id"
@@ -61,6 +62,54 @@ func generateNextOutput(sc int, header http.Header, body []byte) (*NextOutput, e
 	out.DeadlineMs = header.Get(responseHeaderLambdaRuntimeDeadlineMs)
 	out.InvokedFunctionArn = header.Get(responseHeaderLambdaRuntimeInvokedFunctionArn)
 	out.RawEventResponse = body
+
+	return &out, nil
+}
+
+// Runtime makes this request in order to submit a response.
+//
+// document: https://docs.aws.amazon.com/lambda/latest/dg/runtimes-api.html#runtimes-api-response
+func InvocationResponse(ctx context.Context, client alago.AlagoClient, in *ResponseInput) (*ResponseOutput, error) {
+	if in == nil {
+		return nil, fmt.Errorf("ResponseInput is nil")
+	}
+	if in.AWSRequestID == "" {
+		return nil, fmt.Errorf("ResponseInput.AWSRequestID is empty")
+	}
+	if in.Response == nil {
+		return nil, fmt.Errorf("ResponseInput.Response is nil")
+	}
+
+	url := fmt.Sprintf(invocationResponseEndpointFmt, client.Host(), in.AWSRequestID)
+	sc, _, b, err := internal.CallAPI(context.Background(), client, http.MethodPost, url, in.Response)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := generateResponseOutput(sc, b)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func generateResponseOutput(sc int, body []byte) (*ResponseOutput, error) {
+	out := ResponseOutput{}
+	out.StatusCode = sc
+
+	if sc != http.StatusAccepted {
+		var errRes runtime.ErrorResponse
+		if err := json.Unmarshal(body, &errRes); err != nil {
+			return nil, err
+		}
+		out.Error = &errRes
+		return &out, nil
+	}
+
+	if err := json.Unmarshal(body, &out); err != nil {
+		return nil, fmt.Errorf("err:%v, body:%s", err, string(body))
+	}
 
 	return &out, nil
 }
