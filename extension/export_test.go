5b16dd9c0ac4df97eca9c11f706b29d48405e18b
michimani <michimani210@gmail.com>
2023-01-02 17:16:16 +0900

    add test to extension package

--- extension/export_test.go
+++ extension/export_test.go
@@ -1,6 +1,14 @@
 package extension
 
+import "io"
+
 var (
 	Exported_generateRegisterOutput  = generateRegisterOutput
 	Exported_generateEventNextOutput = generateEventNextOutput
 )
+
+type Exported_event = events
+
+func (es *Exported_event) Exported_toRequestBody() (io.Reader, error) {
+	return es.toRequestBody()
+}
